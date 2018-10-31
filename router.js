c79aaafa3972cd4e5350719744d091d42c068eb5
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-10-31 19:33:14 +0800

    added DELETE endpoint for blog posts

--- router.js
+++ router.js
@@ -13,6 +13,12 @@ BlogPosts.create(
     "Lorenzo Borje"
 ); 
 
+BlogPosts.create(
+    "Everything Wrong With Everything Wrong With In 400 Words Or Less", 
+    "Film criticism in the age of YouTube", 
+    "Lorenzo Borje"
+); 
+
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 });
@@ -29,6 +35,14 @@ router.post('/', jsonParser, (req, res) => {
 
     const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
     res.status(201).json(post);
+});
+
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post with id ${req.params.id}`);
+    res.status(204).end();
 })
+
+
 
 module.exports = router;
