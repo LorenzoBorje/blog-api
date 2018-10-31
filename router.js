182b002c463ea87b66ad3e2a68b90671e758f7db
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-10-31 19:27:43 +0800

    added POST endpoint for blog posts

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 const app = express();
+const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models.js');
 
@@ -14,5 +16,19 @@ BlogPosts.create(
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 });
+
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ["title", "content", "author"];
+    requiredFields.forEach(field => {
+        if (!(field in req.body)) {
+            const message = `Missing ${field} in request body`;
+            console.log(message);
+            res.status(400).send(message);
+        }
+    });
+
+    const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+    res.status(201).json(post);
+})
 
 module.exports = router;
